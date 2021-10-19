8edcbe1782a7bb3a8944d3c1106157497edb038d
Mirajna <theastrot@gmail.com>
2021-10-20 01:42:46 +0300

    M's version

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 window.$ = window.jQuery = require('jquery');
+// Mobile menu
+$(document).ready(function () {
+    $('.mobile-menu').on('click', function () {
+        $('.menu').slideToggle().css('right', '0px !important').css('left', 'auto');
+    }); 
+})
+// Anchor links
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
