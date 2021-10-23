b29b0b95941041d4d6691922a1dfac8110ca5f6c
Mirajna <theastrot@gmail.com>
2021-10-23 08:22:09 +0300

    Pagination, new mobile-menu and responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,4 @@
 window.$ = window.jQuery = require('jquery');
-// Mobile menu
-// $(document).ready(function () {
-//     $('.mobile-menu-left').on('click', function () {
-//         $('.menu-left').slideToggle().css('right', '0px !important').css('left', 'auto');
-//     }); 
-//     $('.mobile-menu-right').on('click', function () {
-//         $('.menu-right').slideToggle().css('right', '0px !important').css('left', 'auto');
-//     }); 
-// });
 // Anchor links
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
@@ -37,9 +28,6 @@ $(window).scroll(function () {
     (/*windowPosition >= bannerHeight - header.outerHeight()*/window.pageYOffset > 0) ? header.addClass('sticky') : header.removeClass('sticky');
 });
     //Pagination
-
-//import Swiper from 'swiper';
-
 const swiperReviews = new Swiper('.reviews', {
     loop: false,
     pagination: {
